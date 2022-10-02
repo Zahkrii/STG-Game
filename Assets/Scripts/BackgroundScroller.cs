7654de5a0f0dd88cc08473e229d836b1de19eea3
Zahkrii <32428600+Zahkrii@users.noreply.github.com>
2022-10-02 17:24:48 +0800

    角色移动加背景滚动

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollVelocity;
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset += scrollVelocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+
+    private new Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
+            0.0f);
+    }
+}
